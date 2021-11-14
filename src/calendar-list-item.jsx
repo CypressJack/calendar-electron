e8dd44ef4e1297ed31bc84f5dffdbe865ee9f587
wade <wadecroft@gmail.com>
2021-11-14 14:54:03 -0800

    start building react components

---
+++ src/calendar-list-item.jsx
@@ -0,0 +1,11 @@
+import './style.css';
+
+function CalendarListItem() {
+  return (
+    <div className="calendar-list-item">
+      
+    </div>
+  );
+}
+
+export default CalendarListItem;
---
+++ src/calendar-list.jsx
@@ -0,0 +1,11 @@
+import './style.css';
+
+function CalendarList() {
+  return (
+    <div className="calendar-list">
+      
+    </div>
+  );
+}
+
+export default CalendarList;
